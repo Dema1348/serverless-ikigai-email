38db0f42b67e3a13d1d95a449be2fb8bb010fd03
Edson Pérez Demanet <edson@MacBook-Air-de-Edson.local>
2021-06-19 11:40:36 -0400

    feat: first steps

---
+++ functions/sendEmail.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const sgMail = require("@sendgrid/mail");
+const { SENDGRID_KEY } = process.env;
+
+sgMail.setApiKey(SENDGRID_KEY);
+
+exports.handler = async function (event, context, callback) {
+  const { email, comment, phone } = JSON.parse(event.body);
+
+  const msg = {
+    to: "admin@ikigaisystems.cl",
+    from: "contacto@ikigaisystems.cl",
+    subject: `contactar a ${email}`,
+    text: "contacto",
+    html: `Correo cliente: <strong>${email}</strong><br>
+             Comentario: <strong>${comment}</strong><br> 
+             Telefono: <strong>${phone}</strong><br> `,
+  };
+
+  try {
+    const response = await sgMail.send(msg);
+    console.log(response);
+    callback(null, {
+      statusCode: 200,
+      body: "true",
+    });
+  } catch (error) {
+    callback(null, {
+      statusCode: 500,
+      body: error,
+    });
+  }
+};
